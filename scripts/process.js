af36d0c403c5459ecdea2a8eacb2e26a6369ff70
chico <chi187@gmail.com>
2016-11-16 16:11:41 +0300

    - omit version patch - concat nodejs and  iojs into one array

--- scripts/process.js
+++ scripts/process.js
@@ -1,22 +1,27 @@
 var fs = require('fs');
 var path = require('path');
 
-var pick = require('lodash.pick');
 var semver = require('semver');
 
 var processedData = fs.readdirSync(path.join(__dirname, '../data/raw')).reduce(function (result, file) {
   var envName = path.basename(file, '.json');
   var content = require(path.join(__dirname, '../data/raw', file));
-  result[envName] = content
-    .map(function (env) {
-      return pick(env, ['version', 'date', 'lts']);
+  return result.concat(content
+    .filter(function (env) {
+      return semver.patch(env.version) === 0;
     })
-    .sort(function (a, b) {
-      if (semver.gt(a.version, b.version)) return 1;
-      if (semver.lt(a.version, b.version)) return -1;
-      return 0;
-    });
-  return result;
-}, {});
+    .map(function (env) {
+      return {
+        name: envName,
+        version: env.version.substr(1),
+        date: env.date,
+        lts: env.lts
+      };
+    }));
+}, []).sort(function (a, b) {
+  if (semver.gt(a.version, b.version)) return 1;
+  if (semver.lt(a.version, b.version)) return -1;
+  return 0;
+});
 
 fs.writeFileSync(path.join(__dirname, '../data/processed/envs.json'), JSON.stringify(processedData));
