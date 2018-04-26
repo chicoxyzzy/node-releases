db510a92fbf570b97b6bee0675f5f71336d26a97
vostrik <mail@vostrik.com>
2018-04-26 04:42:06 +0300

    Add loading of Node.js release schedule to fetch.js

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,13 +5,22 @@ var path = require('path');
 // iojs is deprecated and won't be updated anymore
 var envs = ['nodejs'/*, 'iojs'*/];
 
-function update(env) {
-  http.get('https://' + env + '.org/dist/index.json', function (response) {
-    var file = fs.createWriteStream(path.join(__dirname, '../data/raw', env + '.json'));
+envs.forEach(function (env) {
+  loadData({
+    srcUrl: 'https://' + env + '.org/dist/index.json',
+    dstPath: path.resolve(__dirname, '../data/raw', env + '.json')
+  })
+});
+loadData({
+  srcUrl: 'https://raw.githubusercontent.com/nodejs/Release/master/schedule.json',
+  dstPath: path.resolve(__dirname, '../data/release-schedule/release-schedule.json')
+});
+
+function loadData (params) {
+  http.get(params.srcUrl, function (response) {
+    var file = fs.createWriteStream(params.dstPath);
     response.pipe(file);
   }).on('error', function (error) {
     console.error(error.message);
   });
 }
-
-envs.forEach(update);
