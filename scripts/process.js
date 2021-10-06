31afdc6e2b9cd9c3a12c1eb7048c0ceaf024976e
Andrey Sitnik <andrey@sitnik.ru>
2021-10-06 06:35:38 -0400

    Compress JSON

--- scripts/process.js
+++ scripts/process.js
@@ -25,4 +25,4 @@ var processedData = fs.readdirSync(path.join(__dirname, '../data/raw')).reduce(f
   return 0;
 });
 
-fs.writeFileSync(path.join(__dirname, '../data/processed/envs.json'), JSON.stringify(processedData, null, 2));
+fs.writeFileSync(path.join(__dirname, '../data/processed/envs.json'), JSON.stringify(processedData));
